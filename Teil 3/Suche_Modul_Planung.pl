6a9a55fb8864a9c325fa6ce67ed574e30929fe55
Daniel Müller <lincore@hotmail.de>
2015-12-11 17:39:10 +0100

    Added heuristic
    added all_false_under function,
    added eval_path function for second heuristic

--- Teil 3/Suche_Modul_Planung.pl
+++ Teil 3/Suche_Modul_Planung.pl
@@ -1,11 +1,11 @@
-% Die Schnittstelle umfasst
-%   start_description	;Beschreibung des Startzustands
+﻿% Die Schnittstelle umfasst
+%   start_description   ;Beschreibung des Startzustands
 %   start_node          ;Test, ob es sich um einen Startknoten handelt
 %   goal_node           ;Test, ob es sich um einen Zielknoten handelt
 %   state_member        ;Test, ob eine Zustandsbeschreibung in einer Liste
 %                        von Zustandsbeschreibungen enthalten ist
 %   expand              ;Berechnung der Kind-Zustandsbeschreibungen
-%   eval-path		;Bewertung eines Pfades
+%   eval-path           ;Bewertung eines Pfades
 %
 %
 %   (Action, State, Value)
@@ -65,8 +65,10 @@ false_under(Block, State, Intersection, Accu, Result) :- member(on(Under, Block)
 
 % on(block4, block2)
 
-under(HigherBlock, State) :- findall(
-
+%under(HigherBlock, State) :- findall(
+all_false_under(_State,[],Result,Result).
+all_false_under(State,[on(X,Y)|RestIntersection],Accu,_Result):- false_under(X,State,[on(X,Y)|RestIntersection],[],ResultFU),
+                                                                     append(ResultFU,Accu,NewResult),all_false_under(State,RestIntersection,NewResult,NewResult).
 
 start_node((start,_,_)).
 
@@ -89,18 +91,27 @@ state_member(State,[FirstState|_]):- subtract(State, FirstState, []), !.
 %Es ist sichergestellt, dass die beiden ersten Klauseln nicht zutreffen.
 state_member(State,[_|RestStates]):- state_member(State, RestStates).
   %"rekursiver Aufruf".
+  
+%%Kompliziertere Heuristik(funktioniert schlechter): Schaue dir an wie viele Bloecke unter "richtigen" Bloecken liegen und generiere daraus einen Wert
+eval_path([(_,State,Value)|RestPath]):-
+        goal_description(Goal),
+        intersection(Goal, State, Intersection),
+        include(is_on, Intersection, Is_Ons_Intersection),
+        all_false_under(State,Is_Ons_Intersection,[],Result),
+        length(Result,LengthResult),
+        length(RestPath, LengthRestPath),
+        Value is LengthResult+LengthRestPath.
 
-eval_path(
-
+%%Anzahl von noch falschen Zuständen
 eval_path_([(_,State,Value)|RestPath]):-
-	goal_description(Goal),
-	include(is_on, Goal, Is_Ons_Goal),
-	intersection(Goal, State, Intersection),
-	include(is_on, Intersection, Is_Ons_Intersection),
-	length(Is_Ons_Goal, LengthGoal),
-	length(Is_Ons_Intersection, LengthIntersection),
-	length(RestPath, LengthRestPath),
-	Value is LengthGoal - LengthIntersection + LengthRestPath.
+        goal_description(Goal),
+        include(is_on, Goal, Is_Ons_Goal),
+        intersection(Goal, State, Intersection),
+        include(is_on, Intersection, Is_Ons_Intersection),
+        length(Is_Ons_Goal, LengthGoal),
+        length(Is_Ons_Intersection, LengthIntersection),
+        length(RestPath, LengthRestPath),
+        Value is LengthGoal - LengthIntersection + LengthRestPath.
  % eval_state(State,"Rest des Literals bzw. der Klausel"
  % "Value berechnen".
 
@@ -137,20 +148,20 @@ mysubset([H|T],List):-
 
 
 expand_help(State,Name,NewState):-
-  writeln("Start expand help"),
-  action(Name, C, D, A),	% "Action suchen"
+  %writeln("Start expand help"),
+  action(Name, C, D, A),        % "Action suchen"
   %write(" Name: "), writeln(Name),
-  write(" C: "), writeln(C), write(" D "), writeln(D),
-  mysubset(C, State),				       % "Conditions testen"
-  writeln(State),
-  write(" Name: "), writeln(Name),
-  write("C nach mysubset: "), writeln(C),
-  write("D nach subset: "), writeln(D),
-  write("A nach subset: "), writeln(A),
-  subtract(State, D, ResultFromDelete),		       % "Del-List umsetzen"
-  write(" Result from delete: "), writeln(ResultFromDelete),
-  append(ResultFromDelete, A, NewState),
-  write("new state: "), writeln(NewState).       % "Add-List umsetzen".
+  %write(" C: "), writeln(C), write(" D "), writeln(D),
+  mysubset(C, State),                                  % "Conditions testen"
+  %writeln(State),
+  %write(" Name: "), writeln(Name),
+  %write("C nach mysubset: "), writeln(C),
+  %write("D nach subset: "), writeln(D),
+  %write("A nach subset: "), writeln(A),
+  subtract(State, D, ResultFromDelete),                % "Del-List umsetzen"
+  %write(" Result from delete: "), writeln(ResultFromDelete),
+  append(ResultFromDelete, A, NewState).
+  %write("new state: "), writeln(NewState).       % "Add-List umsetzen".
   %list_to_set(ResultFromAppend, NewState).   % we don't need this, yeah?
 expand((_,State,_),Result):-
   findall((Name,NewState,_),expand_help(State,Name,NewState),Result).
